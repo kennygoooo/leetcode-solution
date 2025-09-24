e8cc00310247e276adc3d57e05f81985af6198f4
kennygoooo <keny7946@gmail.com>
2025-09-23 23:46:10 -0400

    Add 2352. hash map

---
+++ 2352. Equal Row and Column Pairs.cpp
@@ -0,0 +1,28 @@
+// 2352. Equal Row and Column Pairs
+
+class Solution {
+public:
+    int equalPairs(vector<vector<int>>& grid) {
+        int n = grid.size();
+        map<vector<int>, int> rowCount;
+
+        for (int i = 0; i < n; i++){
+            rowCount[grid[i]]++;
+        }
+
+        int result = 0;
+
+        for (int j = 0; j < n; j++){
+            vector<int> col;
+
+            for (int i = 0; i < n; i++){
+                col.push_back(grid[i][j]);
+            }
+
+            if (rowCount.find(col) != rowCount.end()){
+                result += rowCount[col]; 
+            }
+        }
+        return result;
+    }
+};
